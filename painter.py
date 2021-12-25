ef76d029c2d203709a7da1708f3d9be9c7eda7d1
Guoker3 <245266575@qq.com>
2021-12-26 06:42:07 +0800

    complte the element

--- painter.py
+++ painter.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 import copy
+from numpy.random import randint
 
 class ShapeGenerator:
     def __init__(self):
@@ -9,7 +10,7 @@ class ShapeGenerator:
         self.cmap = plt.cm.cubehelix
         self.vacancy = 0  # black,where out of screen
         self.blank = 256  # white,where is the screen have nothing to show
-        self.vacancyPoint = self.vacancy #where is a point of the generated Shape,can't equal to zero
+        self.vacancyPoint = copy.deepcopy(self.vacancy) #where is a point of the generated Shape,can't equal to zero
 
     def generateShape(self,shape,maxHeight,maxWidth,args=None):
         canvas=np.zeros((maxHeight,maxWidth))
@@ -78,6 +79,7 @@ class Canvas(ShapeGenerator):
         self.basemat=None
         self.canvas=None
         self.clean=None
+        #self.remain balnk
 
     #generator the max cubic storage mat
     def prepareBoard(self):
@@ -101,12 +103,13 @@ class Canvas(ShapeGenerator):
                     self.canvas[y+h][x+w]=self.vacancy
 
 
-    def paintElement(self,elementMat,y,x):
+    def paintElement(self,element,y,x):
+        elementMat=element.mat
         hh = elementMat.shape[0]
         ww = elementMat.shape[1]
         for h in range(hh):
             for w in range(ww):
-                if elementMat[h][w] == self.vacancyPoint and (y + h) < self.height and (x + w) < self.width:
+                if elementMat[h][w] != element.blank  and (y + h) < self.height and (x + w) < self.width:
                     self.canvas[y + h][x + w] = elementMat[h][w]
         return (y,x)    #return position to Element sample
 
@@ -125,24 +128,57 @@ class Canvas(ShapeGenerator):
                         self.canvas[y + h][x + w] = elementMat[h][w]
 
 class Element(ShapeGenerator):
+
     def __init__(self,type="default"):
+        ShapeGenerator.__init__(self)
         self.type=type
         self.pos=None #(y,x) returned by Canvas sample
         self.hasPainted=False
         self.mat=None
+        self.shapeMat=None
+        self.height=None
+        self.width=None
         if type=="default":
             pass
         else:
             raise Exception("ELEMENT for paint TYPE UNKOWN")
     #def format(self,rawPicture):
     #    pass
-    def generateRandomElementForTest(self):
-        pass
+    def generateRandomElementForTest(self,canvas):
+        maxHeight=canvas.height
+        maxWidth=canvas.width
+        shapes=["rectangle","isosceles triangle","upper triangle","lower triangle","ellipse"]
+        shapeName=shapes[randint(len(shapes))]
+        self.height=randint(int(maxHeight/10)+1,int(maxHeight/3)+1)
+        self.width=randint(int(maxWidth/10)+1,int(maxWidth/3)+1)
+        self.shapeMat=self.generateShape(shapeName,self.height,self.width)
+        self.mat=np.zeros((self.height,self.width))
+        for h in range(self.height):
+            for w in range(self.width):
+                self.mat[h][w]=self.blank
+        #random color
+        for h in range(self.height):
+            for w in range(self.width):
+                if self.shapeMat[h][w]==self.vacancyPoint:
+                    self.mat[h][w]=randint(self.vacancy+1,self.blank)
+
+    def showElement(self,title="element"):
+        tempMat = np.zeros((self.height+1,self.width))
+        for h in range(self.height):
+            for w in range(self.width):
+                tempMat[h][w]=self.mat[h][w]   #default to be paintful
+        #to control and show color reflect in matshow
+        for w in range(self.width):
+            tempMat[self.height][w]=self.vacancy
+        tempMat[self.height][self.width-1]=self.blank
+        plt.matshow(tempMat,cmap=self.cmap)
+        plt.title(title)
+        plt.show()
 
 class Painter(Canvas,Element):
     def __init__(self):
         self.paintedElementList=list()
-        self.waitingPaintList()=list()
+        self.waitingPaintList=list()
 
     def checkIsBlank(self):
         pass
@@ -171,8 +207,8 @@ if __name__ == "__main__":
     #plt.matshow(mat,cmap=plt.cm.cubehelix)
     #plt.show()
 
-    cv=Canvas(60,100)
-    ellipse=cv.generateShape("ellipse",16,30)
+    cv=Canvas(160,100)
+    ellipse=cv.generateShape("ellipse",50,30)
     cv.showShape(ellipse)
 
     cv.prepareBoard()
@@ -180,3 +216,9 @@ if __name__ == "__main__":
 
     cv.tailorCanvas(ellipse,50,50)
     cv.showCanvas(title="after tailor")
+
+    el=Element()
+    el.generateRandomElementForTest(cv)
+    el.showElement()
+    cv.paintElement(el,10,10)
+    cv.showCanvas(title="after paint")
